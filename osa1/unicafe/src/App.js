b02836ec96e9a5e8799a3fb3ab391a9876731e50
nikoruo <83632962+nikoruo@users.noreply.github.com>
2021-05-10 15:19:10 +0300

    step1
    unicafe, tehtävä 1.6 ja kommentit

---
+++ osa1/unicafe/src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+
+//otsikot
+const Header = ({title}) => {
+    return (
+        <h1>
+            {title}
+        </h1>
+    )
+}
+
+//statistics -rivit
+const Stats = ({count, name}) => {
+    return (
+        <p>{name} {count}</p>    
+    )
+}
+
+//nappulat
+const Button = ({ handleClick, name }) => (
+    <button onClick={handleClick}>{name}</button>
+)
+
+const App = () => {
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    //toiminallisuus Good -nappulalle
+    const handleGoodClick = () => setGood(good + 1)
+    //toiminallisuus Neutral -nappulalle
+    const handleNeutralClick = () => setNeutral(neutral + 1)
+    //toiminallisuus Bad -nappulalle
+    const handleBadClick = () => setBad(bad + 1)
+
+    return (
+        <div>
+            <Header title={"give feedback"} />
+            <Button handleClick={handleGoodClick} name={"good"}></Button>
+            <Button handleClick={handleNeutralClick} name={"neutral"}></Button>
+            <Button handleClick={handleBadClick} name={"bad"}></Button>
+            <Header title={"statistics"} />
+            <Stats count={good} name={"good"} />
+            <Stats count={neutral} name={"neutral"} />
+            <Stats count={bad} name={"bad"} />
+        </div>
+    )
+}
+
+export default App
