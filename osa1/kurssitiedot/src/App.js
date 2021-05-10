f6b71a57bdf5836a49b3369b323f79ee8114a9e0
nikoruo <83632962+nikoruo@users.noreply.github.com>
2021-05-10 12:41:28 +0300

    step 3
    kurssitiedot, tehtävä 1.3

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -2,35 +2,34 @@ import React from 'react'
 
 //Komponentti, jolla renderöidään kurssin otsikko
 const Header = (props) => {
+    console.log(props)
     return (
-        <div>
-            <h1>
-                {props.title}
-            </h1>
-        </div>
+        <h1>
+            {props.title}
+        </h1>
     )
 }
 
 //Komponentti, jolla renderöidään kurssin yksittäisten osien tiedot
 //saa propsinaan taulukon, jossa 0: osan nimi, 1: tehtävien määrä
 const Part = (props) => {
+    console.log(props)
     return (
-        <div>
-            <p>
-                {props.table[0]} {props.table[1]}
-            </p>
-        </div>
+        <p>
+            {props.table.name} {props.table.exercises}
+        </p>
     )
 }
 
 //Komponentti, jolla renderöidään kurssin osat
 //saa propsinaan 3x taulukkoa, jossa 0: osan nimi, 1: tehtävien määrä
 const Content = (props) => {
+    console.log(props)
     return (
         <div>
-            <Part table={[props.p1[0], props.p1[1]]} />
-            <Part table={[props.p2[0], props.p2[1]]} />
-            <Part table={[props.p3[0], props.p3[1]]} />
+            <Part table={props.table[0]} />
+            <Part table={props.table[1]} />
+            <Part table={props.table[2]} />
         </div>
     )
 }
@@ -38,12 +37,11 @@ const Content = (props) => {
 
 //Komponentti, jolla renderöidään kurssin nimi
 const Total = (props) => {
+    console.log(props)
     return (
-        <div>
-            <p>
-                Number of exercises {props.sum}
-            </p>
-        </div>
+        <p>
+            Number of exercises {props.sum}
+        </p>
     )
 }
 
@@ -51,18 +49,24 @@ const Total = (props) => {
 //pääkomponentti, joka sisältää datan ja palautettavan div:n
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    const part1 = {
+        name: 'Fundamentals of React',
+        exercises: 10
+    }
+    const part2 = {
+        name: 'Using props to pass data',
+        exercises: 7
+    }
+    const part3 = {
+        name: 'State of a component',
+        exercises: 14
+    }
 
     return (
         <div>
             <Header title={course} />
-            <Content p1={[part1, exercises1]} p2={[part2, exercises2]} p3={[part3, exercises3]}/>
-            <Total sum={exercises1 + exercises2 + exercises3} />
+            <Content table={[part1, part2, part3]}/>
+            <Total sum={part1.exercises + part2.exercises + part3.exercises} />
         </div>
     )
 }
