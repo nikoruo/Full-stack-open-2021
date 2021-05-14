97716c09369f518bcf0aa69c5c860133f948873b
nikoruo <83632962+nikoruo@users.noreply.github.com>
2021-05-14 08:42:29 +0300

    step 6, päivityksiä
    TO illan koodit

--- osa2/kurssitiedot/src/components/Course.js
+++ osa2/kurssitiedot/src/components/Course.js
@@ -2,8 +2,7 @@ import React from 'react'
 
 //Alikomponentti, jolla renderöidään kurssin otsikko
 const Header = (props) => {
-    console.log("Header")
-    console.log(props)
+    console.log("Header", props)
     return (
         <h1>
             {props.title.name}
@@ -14,8 +13,7 @@ const Header = (props) => {
 //Alikomponentti, jolla renderöidään kurssin yksittäisten osien tiedot
 //saa propsinaan tiedot yksittäisestä osasta
 const Part = (props) => {
-    console.log("Part")
-    console.log(props)
+    console.log("Part", props)
     return (
         <p>
             {props.obj.name} {props.obj.exercises}
@@ -25,8 +23,7 @@ const Part = (props) => {
 
 //Alikomponentti, jolla renderöidään kurssin osat
 const Content = (props) => {
-    console.log("Content")
-    console.log(props)
+    console.log("Content", props)
     return (
         <div>
             <Part obj={props.table.parts[0]} />
@@ -39,8 +36,7 @@ const Content = (props) => {
 
 //Alikomponentti, jolla renderöidään kurssin tehtävien määrä
 const Total = (props) => {
-    console.log("Total")
-    console.log(props)
+    console.log("Total", props)
     return (
         <p>
             Number of exercises {props.sum.parts[0].exercises + props.sum.parts[1].exercises + props.sum.parts[2].exercises}
@@ -49,7 +45,7 @@ const Total = (props) => {
 }
 
 const Course = ({ course }) => {
-    console.log("Course -component")
+    console.log("Course -component", course)
     return (
         <div>
             <Header title={course} />
