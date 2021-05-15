5b6a864538d73776bcf2e1e027a6036417cbfb37
nikoruo <83632962+nikoruo@users.noreply.github.com>
2021-05-15 18:06:06 +0300

    puhelinluettelo, step1
    T2.6 valmis

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,23 +2,40 @@ import React, { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
+        { id:1, name: 'Arto Hellas' }
     ])
     const [newName, setNewName] = useState('')
 
+    const addContact = (event) => {
+        event.preventDefault()
+        console.log('Added new name')
+        const newContact = {
+            id: persons.length + 1,
+            name: newName
+        }
+        setPersons(persons.concat(newContact))
+        setNewName('')
+    }
+
+    const handleNameChange = (event) => {
+        console.log(event.target.value)
+        setNewName(event.target.value)
+    }
+
+    
     return (
         <div>
             <h2>Phonebook</h2>
-            <form>
+            <form onSubmit={addContact}>
                 <div>
-                    name: <input />
+                    name: <input value={newName} onChange={handleNameChange}/>
                 </div>
                 <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
-      ...
+            {persons.map(person => <p key={person.id}>{person.name}</p>)}
         </div>
     )
 
