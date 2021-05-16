b5adf9f07761830f5e036e6f224098b9808c29c0
nikoruo <83632962+nikoruo@users.noreply.github.com>
2021-05-16 14:24:38 +0300

    puhelinluettelo, step6
    T2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,18 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
+import axios from 'axios'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { id: 1, name: 'Arto Hellas', number: '040-123456' },
-        { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
-        { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
-        { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [newFilter, setNewFilter] = useState('')
+
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                console.log('promise fulfilled')
+                setPersons(response.data)
+            })
+    }, [])
 
     //käyttäjän lisäys
     const addContact = (event) => {
