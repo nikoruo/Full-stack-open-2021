1e66773056382e365de58532644a95bd47ffca3d
nikoruo <83632962+nikoruo@users.noreply.github.com>
2021-05-23 14:10:30 +0300

    puhelinluettelo_backend, step2
    T3.2

--- osa3/puhelinluettelon_backend/index.js
+++ osa3/puhelinluettelon_backend/index.js
@@ -23,10 +23,14 @@ let persons = [
         id: 4,
         name: "Mary Poppendick",
         number: "39-23-6423122"
-    },
+    }
 ]
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (req, res) => {
+    res.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${new Date()}</p>`)
 })
 
 app.get('/api/persons', (req, res) => {
