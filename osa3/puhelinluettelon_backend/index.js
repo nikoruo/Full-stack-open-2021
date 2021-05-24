a2b746334e37e1dcda26ee05c1d87d5fe80b98a2
nikoruo <83632962+nikoruo@users.noreply.github.com>
2021-05-24 16:21:40 +0300

    puhelinluettelo_backend, step9
    T3.9

--- osa3/puhelinluettelon_backend/index.js
+++ osa3/puhelinluettelon_backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan('tiny', {
