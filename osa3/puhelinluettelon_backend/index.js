f7c013266296cf0e9fb12938ac70679cb553f840
Niko Ruohonen <niko.o.ruohonen@gmail.com>
2021-05-26 10:33:30 +0300

    puhelinluettelo ja tietokanta, step2, step3, step4 ja step5
    Tehtävät 3.14-3.17

--- osa3/puhelinluettelon_backend/index.js
+++ osa3/puhelinluettelon_backend/index.js
@@ -6,9 +6,9 @@ const cors = require('cors')
 const Contact = require('./models/contact')
 
 app.use(cors())
-app.use(express.json())
 app.use(express.static('build'))
-
+app.use(express.json())
+//alla request logger
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan('tiny', {
     skip: function (req, res) { return req.method === "POST" }
@@ -17,67 +17,62 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
     skip: function (req, res) { return req.method !== "POST" }
 }))
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    }
-]
+let persons = []
 
-
-
+//info -sivu
 app.get('/info', (req, res) => {
     res.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${new Date()}</p>`)
 })
 
+//kaikkien kontaktien haku
 app.get('/api/persons', (req, res) => {
     Contact.find({}).then(contacts => {
 		res.json(contacts)
 	})
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(p => p.id === id)
-	Contact.findById(req.params.id).then(contact => {
-		response.json(contact)
+//yksittäisen kontaktin tiedot
+app.get('/api/persons/:id', (req, res, next) => {
+	Contact.findById(req.params.id)
+	.then(contact => {
+		if (contact) {
+			res.json(contact)
+		} else {
+			res.status(404).end()
+		}
 	})
-    /*if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }*/
-
+	.catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+//kontaktin poistaminen
+app.delete('/api/persons/:id', (req, res) => {
+    Contact.findByIdAndRemove(req.params.id)
+	.then(result => {
+		persons = persons.filter(person => person.id !== id)
+		res.status(204).end()
+	})
+	.catch(error => next(error))    
+})
+//kontaktikn numeron päivittäminen
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
 
-    response.status(204).end()
+  const contact = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Contact.findByIdAndUpdate(req.params.id, contact, { new: true })
+    .then(updatedContact => {
+      res.json(updatedContact)
+    })
+    .catch(error => next(error))
 })
 
-const generateId = () => {
+//id generaattori
+const generateId = () => {return Math.floor(Math.random()*1234567890)}
 
-    return Math.floor(Math.random()*1234567890)
-}
-
+//uuden kontaktin luominen
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
@@ -106,6 +101,24 @@ app.post('/api/persons', (request, response) => {
 	
     persons = persons.concat(contact)
 })
+
+// olemattomien osoitteiden käsittely
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+// virheellisten pyyntöjen käsittely
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
