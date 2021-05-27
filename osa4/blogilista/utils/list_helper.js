7ffc4793a9a68afa8040ef36a8f7117706862c37
Niko Ruohonen <niko.o.ruohonen@gmail.com>
2021-05-27 16:15:48 +0300

    apufunktioita ja yksikkötestejä, step3
    T4.5 valmis

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,7 +1,9 @@
+//dummy
 const dummy = (blogs) => {
   return 1
 }
 
+//tykkäyksiä yhteensä
 const totalLikes = (blogs) => {
   const reducer = (sum, item) => {
     return sum + item
@@ -11,7 +13,28 @@ const totalLikes = (blogs) => {
     : blogs.map(blog => blog.likes).reduce(reducer, 0)
 }
 
+//eniten tykkäyksiä
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return undefined
+  } else {
+    const reducer = (max, item) => {
+      return item.likes >= max.likes
+        ? item
+        : max
+    }
+    const current = blogs.reduce(reducer, blogs[0])
+
+    return {
+      'title': current.title,
+      'author': current.author,
+      'likes':current.likes
+    }
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
