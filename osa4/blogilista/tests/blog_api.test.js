2c18adcb1c6311df31a427a14369e6bd4c26b38c
Niko Ruohonen <niko.o.ruohonen@gmail.com>
2021-06-01 12:44:45 +0300

    blogilistan laajennus, step10 ja step11
    T4.22 ja T4.23 valmiit ja toimivat

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -3,16 +3,36 @@ const supertest = require('supertest')
 const app = require('../app')
 const helper = require('./test_helper')
 const api = supertest(app)
-
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 
 describe('when there is initially some blogs saved', () => {
   beforeEach(async () => {
+    await User.deleteMany({})
     await Blog.deleteMany({})
+
+    const user = new User({
+      username: 'blog',
+      password: 'testi'
+    })
+    await user.save()
+
+    const blog = new Blog({
+      title: 'blog with user',
+      author: 'blog_api.test.js',
+      url: 'test.oi',
+      likes: 10,
+      user: user._id
+    })
+    await blog.save()
+
     await Blog.insertMany(helper.initialBlogs)
+
+
   })
 
-  //testejä backendiin
+  //yleisiä testejä
   describe('blog GET general tests', () => {
     test('blogs are returned as json', async () => {
       await api
@@ -24,7 +44,7 @@ describe('when there is initially some blogs saved', () => {
     test('all blogs are returned', async () => {
       const response = await api.get('/api/blogs')
 
-      expect(response.body).toHaveLength(helper.initialBlogs.length)
+      expect(response.body).toHaveLength(helper.initialBlogs.length+1)
     })
 
     test('all blogs are identified by id', async () => {
@@ -34,6 +54,7 @@ describe('when there is initially some blogs saved', () => {
     })
   })
 
+  //blogien hakeminen
   describe('blog GET specific blog tests', () => {
     test('succeeds with a valid id', async () => {
       const blogsAtStart = await helper.blogsInDb()
@@ -67,6 +88,7 @@ describe('when there is initially some blogs saved', () => {
     })
   })
 
+  //blogin lisääminen
   describe('blog POST tests', () => {
     test('a valid blog can be added ', async () => {
 
@@ -78,8 +100,15 @@ describe('when there is initially some blogs saved', () => {
         user: helper.initialUsers[0].id
       }
 
+      const user = await helper.usersInDb()
+      const token = await jwt.sign(
+        user[0],
+        process.env.SECRET
+      )
+
       await api
         .post('/api/blogs')
+        .set('Authorization', `bearer ${token}`)
         .send(newBlog)
         .expect(201)
         .expect('Content-Type', /application\/json/)
@@ -88,7 +117,7 @@ describe('when there is initially some blogs saved', () => {
 
       const blogs = blogsAtEnd.map(r => r.title)
 
-      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 2)
       expect(blogs).toContain(
         'async/await simplifies making async calls'
       )
@@ -100,8 +129,15 @@ describe('when there is initially some blogs saved', () => {
         url: 'https://developer.cdn.mozilla.net/en-US/docs/Learn/JavaScript/Asynchronous/Async_await'
       }
 
+      const user = await helper.usersInDb()
+      const token = await jwt.sign(
+        user[0],
+        process.env.SECRET
+      )
+
       await api
         .post('/api/blogs')
+        .set('Authorization', `bearer ${token}`)
         .send(newBlog)
         .expect(201)
         .expect('Content-Type', /application\/json/)
@@ -110,7 +146,7 @@ describe('when there is initially some blogs saved', () => {
 
       const blogs = blogsAtEnd.map(r => r.title)
 
-      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 2)
       expect(blogs).toContain(
         'async/await simplifies making async calls'
       )
@@ -121,29 +157,61 @@ describe('when there is initially some blogs saved', () => {
         likes: 100
       }
 
+      const user = await helper.usersInDb()
+      const token = await jwt.sign(
+        user[0],
+        process.env.SECRET
+      )
+
       await api
         .post('/api/blogs')
+        .set('Authorization', `bearer ${token}`)
         .send(newBlog)
         .expect(400)
 
       const blogsAtEnd = await helper.blogsInDb()
 
-      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length+1)
+    })
+    test('fails with status code 401 if unauthorized', async () => {
+      const newBlog = {
+        title: 'async/await simplifies making async calls',
+        author: 'blog POST test',
+        url: 'https://developer.cdn.mozilla.net/en-US/docs/Learn/JavaScript/Asynchronous/Async_await'
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(401)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length+1)
     })
   })
+
+  //blogin poistaminen
   describe('deletion of a blog', () => {
     test('succeeds with status code 204 if id is valid', async () => {
       const blogsAtStart = await helper.blogsInDb()
       const blogToDelete = blogsAtStart[0]
 
+      const user = await helper.usersInDb()
+      const token = await jwt.sign(
+        user[0],
+        process.env.SECRET
+      )
+
       await api
         .delete(`/api/blogs/${blogToDelete.id}`)
+        .set('Authorization', `bearer ${token}`)
         .expect(204)
 
       const blogsAtEnd = await helper.blogsInDb()
 
       expect(blogsAtEnd).toHaveLength(
-        helper.initialBlogs.length - 1
+        helper.initialBlogs.length
       )
 
       const contents = blogsAtEnd.map(r => r.title)
@@ -151,6 +219,8 @@ describe('when there is initially some blogs saved', () => {
       expect(contents).not.toContain(blogToDelete.title)
     })
   })
+
+  //blogin päivittäminen
   describe('updating of a blog', () => {
     test('succeeds with status code 204', async () => {
       const blogsAtStart = await helper.blogsInDb()
@@ -159,7 +229,8 @@ describe('when there is initially some blogs saved', () => {
         author: blogsAtStart[0].author,
         url: blogsAtStart[0].url,
         likes: blogsAtStart[0].likes + 50,
-        id: blogsAtStart[0].id
+        id: blogsAtStart[0].id,
+        user: blogsAtStart[0].user
       }
 
       await api
@@ -169,23 +240,26 @@ describe('when there is initially some blogs saved', () => {
 
       const blogsAtEnd = await helper.blogsInDb()
 
-      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length+1)
       expect(blogToUpdate.likes).toBe(blogsAtEnd[0].likes)
     })
     test('fails with status code 400 if data invalid', async () => {
-      const newBlog = {
-        url: 'https://developer.cdn.mozilla.net/en-US/docs/Learn/JavaScript/Asynchronous/Async_await',
-        likes: 100
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToUpdate = {
+        url: blogsAtStart[0].url,
+        likes: blogsAtStart[0].likes + 50,
+        id: blogsAtStart[0].id,
+        user: blogsAtStart[0].user
       }
 
       await api
-        .post('/api/blogs')
-        .send(newBlog)
+        .put(`/api/blogs/${blogToUpdate.id}`)
+        .send(blogToUpdate)
         .expect(400)
 
       const blogsAtEnd = await helper.blogsInDb()
 
-      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length+1)
     })
   })
 })
