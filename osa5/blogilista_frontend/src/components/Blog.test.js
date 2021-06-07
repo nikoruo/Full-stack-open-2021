29802351647a067cd06298c34c8843c457c7fcce
Niko Ruohonen <niko.o.ruohonen@gmail.com>
2021-06-07 08:25:57 +0300

    blogilistan testit, step2
    T5.14 valmis

--- osa5/blogilista_frontend/src/components/Blog.test.js
+++ osa5/blogilista_frontend/src/components/Blog.test.js
@@ -1,32 +1,65 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders content', () => {
-  const blog = {
-    title : 'Component testing is done with react-testing-library',
-    author: 'test',
-    url: 'url',
-    likes: 10
-  }
 
-  const component = render(
-    <Blog blog={blog} />
-  )
+describe('<Blog />', () => {
+  test('visibility = false, renders content', () => {
+    const blog = {
+      title: 'Component testing is done with react-testing-library',
+      author: 'test',
+      url: 'url',
+      likes: 10
+    }
 
-  component.debug()
+    const component = render(
+      <Blog blog={blog} />
+    )
 
-  expect(component.container).toHaveTextContent(
-    'Component testing is done with react-testing-library'
-  )
-  expect(component.container).not.toHaveTextContent(
-    'url'
-  )
-  expect(component.container).not.toHaveTextContent(
-    '10'
-  )
-  expect(component.container).toHaveTextContent(
-    'test'
-  )
+    component.debug()
+
+    expect(component.container).toHaveTextContent(
+      'Component testing is done with react-testing-library'
+    )
+    expect(component.container).not.toHaveTextContent(
+      'url'
+    )
+    expect(component.container).not.toHaveTextContent(
+      '10'
+    )
+    expect(component.container).toHaveTextContent(
+      'test'
+    )
+  })
+
+  test('visibility = true, renders content', async () => {
+    const blog = {
+      title: 'Component testing is done with react-testing-library',
+      author: 'test',
+      url: 'url',
+      likes: 10,
+      user: {
+        name: 'fireEvent'
+      }
+    }
+    const user = {
+      username: 'fireEvent'
+    }
+
+
+    const component = render(
+      <Blog blog={blog} user={user} />
+    )
+
+    const button = component.getByText('view')
+    fireEvent.click(button)
+
+    expect(component.container).toHaveTextContent(
+      'url'
+    )
+    expect(component.container).toHaveTextContent(
+      '10'
+    )
+  })
 })
