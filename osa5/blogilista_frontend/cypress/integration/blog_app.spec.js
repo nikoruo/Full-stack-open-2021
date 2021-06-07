70e5a3eb3af36a064cd4bcf5fd422f73da32c90d
Niko Ruohonen <niko.o.ruohonen@gmail.com>
2021-06-07 13:53:32 +0300

    blogilistan end to end -testit, step3
    T5.19 valmis

--- osa5/blogilista_frontend/cypress/integration/blog_app.spec.js
+++ osa5/blogilista_frontend/cypress/integration/blog_app.spec.js
@@ -38,4 +38,22 @@ describe('Blog app', function () {
         .should('have.css', 'color', 'rgb(255, 0, 0)')
     })
   })
+
+  //blogin luominen
+  describe('When logged in', function () {
+    beforeEach(function () {
+      cy.login({ username: 'cypress', password: 'secret', id:'60bdf935f30f31361c439b08' })
+    })
+
+    it('A blog can be created', function () {
+      cy.createBlog({
+        title: 'blog to be created',
+        author: 'cy',
+        url: 'no need',
+        user: '60bdf935f30f31361c439b08'
+      })
+
+      cy.get('#blogForm').should('contain', 'blog to be created cy')
+    })
+  })
 })
--- osa5/blogilista_frontend/cypress/support/commands.js
+++ osa5/blogilista_frontend/cypress/support/commands.js
@@ -23,3 +23,33 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+//sisäänkirjautuminen
+Cypress.Commands.add('login', ({ username, password }) => {
+  cy.request('POST', 'http://localhost:3003/api/login', {
+    username, password
+  }).then(({ body }) => {
+    localStorage.setItem('loggedBlogappUser', JSON.stringify(body))
+    cy.visit('http://localhost:3000')
+  })
+})
+
+//uuden blogin luominen
+Cypress.Commands.add('createBlog', ({ title, author, url, user }) => {
+  cy.contains('create new blog').click()
+  cy.request({
+    url: 'http://localhost:3003/api/blogs',
+    method: 'POST',
+    body: {
+      title,
+      author,
+      url,
+      user: user.id
+    },
+    headers: {
+      'Authorization': `bearer ${JSON.parse(localStorage.getItem('loggedBlogappUser')).token}`
+    }
+  })
+
+  cy.visit('http://localhost:3000')
+})
