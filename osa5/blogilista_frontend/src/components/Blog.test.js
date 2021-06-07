f130b84d3a3a78cadef5c97290af34f78ed7dc7c
Niko Ruohonen <niko.o.ruohonen@gmail.com>
2021-06-07 08:07:52 +0300

    blogilistan testit, step1
    T5.13 valmis

--- osa5/blogilista_frontend/src/components/Blog.test.js
+++ osa5/blogilista_frontend/src/components/Blog.test.js
@@ -7,13 +7,26 @@ test('renders content', () => {
   const blog = {
     title : 'Component testing is done with react-testing-library',
     author: 'test',
+    url: 'url',
+    likes: 10
   }
 
   const component = render(
     <Blog blog={blog} />
   )
 
+  component.debug()
+
   expect(component.container).toHaveTextContent(
     'Component testing is done with react-testing-library'
+  )
+  expect(component.container).not.toHaveTextContent(
+    'url'
+  )
+  expect(component.container).not.toHaveTextContent(
+    '10'
+  )
+  expect(component.container).toHaveTextContent(
+    'test'
   )
 })
