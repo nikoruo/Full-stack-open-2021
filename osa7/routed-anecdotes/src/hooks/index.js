9533bbf8c168b3300ef0fac418cbeeb654bf870f
Niko Ruohonen <niko.o.ruohonen@gmail.com>
2021-06-14 13:03:01 +0300

    anekdoottisovellus ja hookit step1
    T7.4 valmis

---
+++ osa7/routed-anecdotes/src/hooks/index.js
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+// moduulissa voi olla monta nimettyä eksportia
+export const useAnotherHook = () => {
+  // ...
+}
