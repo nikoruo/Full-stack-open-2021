b513c0c7da69c31c40808c33b57d4ebf7beaaaf5
Niko Ruohonen <niko.o.ruohonen@gmail.com>
2021-09-14 10:11:55 +0300

    8.15 Tietokanta, osa 3
    validointivirheiden käsittely, sekä kovakoodattujen taulukoiden poisto koodista

--- osa8/kirjasto-backend/index.js
+++ osa8/kirjasto-backend/index.js
@@ -6,7 +6,6 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 const Book = require('./models/book')
 const Author = require('./models/author')
-const { graphql } = require('graphql')
 
 const MONGODB_URI = process.env.MONGODB_URI
 
@@ -17,89 +16,6 @@ const MONGODB_URI = process.env.MONGODB_URI
     .catch((error) => {
       console.log('error connection to MongoDB:', error.message)
     })
-
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  { 
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
-
-/*
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
-*/
-
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
 
 //GraphQL skeemat
 const typeDefs = gql`
@@ -177,22 +93,42 @@ const resolvers = {
 
       if (!author) {
         author = new Author({ name: args.author, id: uuid(), bookCount: 1 })
-        await author.save()
+        try {
+          await author.save()
+        } catch (error){
+          throw new UserInputError(error.message, {
+            invalidArgs: args,
+          })
+        }               
       }
 
-      const book = new Book({ ...args, id: uuid(), author: author.id })
-
-      return book.save()
+      const book = new Book({ ...args, id: uuid(), author: author })
+      try {
+        await book.save()
+      } catch (error){
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        })
+      }
+      return book
     },
 
     editAuthor: async (root, args) => {
-      const author = await Author.findOne({ 'name': args.name })
+      let author = await Author.findOne({ 'name': args.name })
 
       if (!author) {
         return null
       }
 
-      return await Author.findOneAndUpdate({ name: author.name }, { born: args.setBornTo }, {new: true})
+      try {
+        author = await Author.findOneAndUpdate({ name: author.name }, { born: args.setBornTo }, { new: true })
+      } catch (error){
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        })
+      }
+
+      return author
     }
   }
 }
