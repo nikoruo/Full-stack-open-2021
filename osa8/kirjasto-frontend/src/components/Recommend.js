8cac78f1737f5c378fb3c046438ce0e1b899462d
Niko Ruohonen <niko.o.ruohonen@gmail.com>
2021-10-11 14:32:20 +0300

    8.22 Välimuistin ajantasaisuus
    Books -näkymä päivittyy hyvin, Recommended -näkymässä käytetty pollausta ratkaisemaan päivittyminen

--- osa8/kirjasto-frontend/src/components/Recommend.js
+++ osa8/kirjasto-frontend/src/components/Recommend.js
@@ -4,31 +4,25 @@ import { ME, ALL_BOOKS } from '../queries'
 
 const Recommend = (props) => {
 
+  let user = useQuery(ME)
+
   const [fGenre, setFGenre] = useState(null)
-  const [getRBooks, result] = useLazyQuery(ALL_BOOKS)
+  const [getRBooks, result] = useLazyQuery(ALL_BOOKS, { pollInterval: 5000 })
 
   const [fBooks, setFBooks] = useState([])
 
-  let user = useQuery(ME)
-
   useEffect(() => {
-    if (user.data && user.data.me) {
+    if (!user.loading && user.data && user.data.me) {
       setFGenre(user.data.me.favoriteGenre)
+      getRBooks({ variables: { genre: user.data.me.favoriteGenre } })
     }
   }, [user])
-
-  useEffect(() => {
-    if (!user.loading) {
-      getRBooks({ variables: { genre: fGenre } })
-    }
-  }, [fGenre])
 
   useEffect(() => {
     if (result.data && result.data.allBooks) {
       setFBooks(result.data.allBooks || [])
     }
   }, [result])
-
 
   if (!props.show) {
     return null
