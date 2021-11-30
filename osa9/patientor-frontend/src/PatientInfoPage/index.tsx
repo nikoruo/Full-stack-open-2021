876ee0e0ca805007fddbcc0110b6ae27b2739135
Niko Ruohonen <niko.o.ruohonen@gmail.com>
2021-11-30 17:56:20 +0200

    9.17: patientor, step2
    /patients/:id toimii frontissa

---
+++ osa9/patientor-frontend/src/PatientInfoPage/index.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+import { Icon } from 'semantic-ui-react';
+
+import { Patient } from "../types";
+import { apiBaseUrl } from "../constants";
+import { useStateValue } from "../state";
+
+const PatientInfoPage = () => {
+  const [{ patient }, dispatch] = useStateValue();
+
+  const { id } = useParams<{ id: string }>();
+  
+  React.useEffect(() => {  
+    const fetchPatientInfo = async () => {
+
+        try {
+        const { data: patientInfo } = await axios.get<Patient>(
+          `${apiBaseUrl}/patients/${id}`
+        );
+        dispatch({ type: "SET_PATIENT_INFO", payload: patientInfo });
+      } catch (error: unknown) {
+        let errorMessage = 'Something went wrong.';
+        if(axios.isAxiosError(error) && error.response) {
+          errorMessage += ' Error: ' + String(error.response.data.message);
+        }
+        console.error(errorMessage);
+      }
+    };
+
+    if(!patient || patient?.id !== id){
+      void fetchPatientInfo();
+    }
+    
+  }, [id]);
+
+  const patientGender = () => {
+    switch(patient?.gender){
+        case "male":
+            return <Icon name="mars" size="big"/>;
+        case "female":
+            return <Icon name="venus" size="big"/>;
+        case "other":
+            return <Icon name="genderless" size="big"/>;
+        default:
+            return null;
+        }
+    };
+
+  return (
+    <div className="App">
+      <h2>{patient?.name} {patientGender()}</h2>
+      <p>ssn: {patient?.ssn}</p>
+      <p>occupation: {patient?.occupation}</p>
+    </div>
+  );
+};
+
+export default PatientInfoPage;
